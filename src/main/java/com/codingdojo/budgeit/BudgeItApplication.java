2eb8bf8690cc2681a0f71a50a00d9fb58c237484
Shivani Autar <shivaniautar18@gmail.com>
2020-10-19 20:55:32 -0700

    first commit

---
+++ src/main/java/com/codingdojo/budgeit/BudgeItApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.budgeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgeItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/budgeit/BudgeItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.budgeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgeItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
